6d130bc06f256391a02a96c45a4916347088415a
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-06-24 11:37:32 -0300

    Add files via upload

---
+++ Davi_24621.py
@@ -0,0 +1,44 @@
+#Question2
+Lista = ["Brasil", "EUA", "Holanda", "França"]
+
+#Question3
+print(Lista)
+
+#Question4
+for item in Lista:
+    print(item)
+
+#question5
+for item in Lista:
+    print(item + item)
+
+#Question6
+for item in Lista:
+    print(item.upper())
+
+#Question7
+
+#Question8
+
+#Question9
+
+#Question10
+
+#Question11
+numbers = list(range(1,11))
+
+#Question12
+print(numbers)
+
+#Question13
+for item in Lista:
+    print("Davi" *10)
+
+#Question14
+
+#Question15
+for item in Lista:
+    print(10, 0, -2)
+    
+
+    
